d63d8dbcbc350b97be7fbeb0fff4e1e26cad9444
jpchupaprecios <juanbarla+chupaprecios@gmail.com>
2025-01-31 16:51:17 -0300

    cambios

--- resources/views/pages/details/components/breadcrumb.blade.php
+++ resources/views/pages/details/components/breadcrumb.blade.php
@@ -1,17 +1,5 @@
 <nav aria-label="breadcrumb" class="mb-6">
-    <ol class="flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5">
+    <ol class="flat-breadcrumbs flex flex-wrap items-center gap-1.5 break-words text-sm text-muted-foreground sm:gap-2.5">
         <li class="inline-flex items-center gap-1.5"><a class="transition-colors hover:text-foreground" href="/">Inicio</a></li>
-        <li aria-hidden="true" class="[&amp;>svg]:h-3.5 [&amp;>svg]:w-3.5" role="presentation">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right h-4 w-4">
-                <path d="m9 18 6-6-6-6"></path>
-            </svg>
-        </li>
-        <li class="inline-flex items-center gap-1.5"><a class="transition-colors hover:text-foreground" href="/categoria/electronica">Electrónica</a></li>
-        <li aria-hidden="true" class="[&amp;>svg]:h-3.5 [&amp;>svg]:w-3.5" role="presentation">
-            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right h-4 w-4">
-                <path d="m9 18 6-6-6-6"></path>
-            </svg>
-        </li>
-        <li class="inline-flex items-center gap-1.5"><span aria-current="page" aria-disabled="true" class="font-normal text-foreground" role="link">Móviles</span></li>
     </ol>
 </nav>
--- resources/views/pages/details/index2.blade.php
+++ resources/views/pages/details/index2.blade.php
@@ -127,6 +127,25 @@
                     }
                 }
             }
+            if (result && typeof result.breadcrumbs_flat !== "undefined") {
+                let bcs = result.breadcrumbs_flat.split(">");
+
+                let html = `<li class="inline-flex items-center gap-1.5"><a class="transition-colors hover:text-foreground" href="/">Inicio</a></li>`;
+                for(var i = 0; i < bcs.length; i++){
+                    if(i > 5){
+                        break;
+                    }
+                    let bc = bcs[i];
+                    html += `<li aria-hidden="true" class="[&amp;>svg]:h-3.5 [&amp;>svg]:w-3.5" role="presentation">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right h-4 w-4">
+                        <path d="m9 18 6-6-6-6"></path>
+                </svg>
+                </li>
+                    <li class="inline-flex items-center gap-1.5"><a class="transition-colors hover:text-foreground" href="/categoria/electronica">` + bc + `</a></li>`;
+                }
+
+                document.querySelector('.flat-breadcrumbs').innerHTML = html;
+            }
         }
     }
 
