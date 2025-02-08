b13e888c7e8a5925043e10fb038121f9efbaba48
jotapey <juanbarla+chupaprecios@gmail.com>
2025-02-07 23:49:35 -0300

    c

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -349,12 +349,12 @@ class ProductController extends Controller
                 'user' => 'user-chupaprecios_lDWEa-country-US',
                 'pass' => '+Aq1w2e3r4t5'
             ],
-            [
+            /*[
                 'host' => 'pr.oxylabs.io',
                 'port' => 7777,
                 'user' => 'customer-jotapey3_qcf4a-cc-us',
                 'pass' => '+Aq1w2e3r4t5'
-            ],
+            ],*/
         ];
 
         $multiCurl = curl_multi_init();
